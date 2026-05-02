30fbb951ee7c0fb5b16fa3e4721ecdd993bce9fb
Osman Taha Çakırcı <otahacakirci@protonmail.com>
2026-05-02 12:37:58 +0300

    chore: initialize Spring Boot project

---
+++ src/main/java/com/ecommerce/project/MyEcomProjectApplication.java
@@ -0,0 +1,13 @@
+package com.ecommerce.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyEcomProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyEcomProjectApplication.class, args);
+	}
+
+}
