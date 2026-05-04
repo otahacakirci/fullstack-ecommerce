8a8db5568e8e0716b43973022d28d5812999667f
Osman Taha Çakırcı <otahacakirci@protonmail.com>
2026-05-04 10:02:47 +0300

    feat(category): add delete method to CategoryController, Service, and Impl

--- src/main/java/com/ecommerce/project/service/CategoryServiceImpl.java
+++ src/main/java/com/ecommerce/project/service/CategoryServiceImpl.java
@@ -18,7 +18,21 @@ public class CategoryServiceImpl implements CategoryService {
 
     @Override
     public void createCategory(Category category) {
+        category.setCategoryId((long) categories.size() + 1);
         categories.add(category);
+    }
+
+    @Override
+    public String deleteCategory(Long categoryId) {
+        Category category = categories.stream().filter(c -> c.getCategoryId().equals(categoryId))
+                .findFirst().orElse(null);
+        if (category == null) {
+            return "Category not found";
+        }
+
+        categories.remove(category);
+        return "Category with categoryId: " + categoryId + " has been deleted";
+
 
     }
 }
