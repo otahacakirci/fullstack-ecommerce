197f7fc45c487706543660b4ad1f2935c981ce70
Osman Taha Çakırcı <otahacakirci@protonmail.com>
2026-05-12 20:25:55 +0300

    refactor(category): remove try-catch blocks and clean up unused imports and dummy list

--- src/main/java/com/ecommerce/project/category/CategoryController.java
+++ src/main/java/com/ecommerce/project/category/CategoryController.java
@@ -35,25 +35,15 @@ public class CategoryController {
 
     @DeleteMapping("/api/admin/categories/{categoryId}")
     public ResponseEntity<String> deleteCategory(@PathVariable Long categoryId) {
-        try {
             String status = categoryService.deleteCategory(categoryId);
             return new ResponseEntity<>(status, HttpStatus.OK);
-        }
-        catch (ResponseStatusException e) {
-            return new ResponseEntity<>(e.getReason(), e.getStatusCode());
-        }
     }
 
     @PutMapping("/api/public/categories/{categoryId}")
     public ResponseEntity<String> updateCategory(@Valid @RequestBody Category category,
                                                  @PathVariable Long categoryId) {
-        try {
             Category savedCategory = categoryService.updateCategory(category, categoryId);
             return new ResponseEntity<>("Category with category id : {%d} has been updated.".formatted(categoryId), HttpStatus.OK);
-        }
-        catch (ResponseStatusException e) {
-            return new ResponseEntity<>(e.getReason(), e.getStatusCode());
-        }
     }
 
 
