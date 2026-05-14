f27fbc6416f97eacd83bb787511e172ecf219bf4
Osman Taha Çakırcı <otahacakirci@protonmail.com>
2026-05-14 18:16:44 +0300

    feat: add Product domain with entity, repository, service and controller
    - Create Product entity with fields
    - Create ProductRepository
    - Create ProductService interface and ProductServiceImpl
    - Create ProductController

--- src/main/java/com/ecommerce/project/controller/CategoryController.java
+++ src/main/java/com/ecommerce/project/controller/CategoryController.java
@@ -1,7 +1,6 @@
-package com.ecommerce.project.category;
+package com.ecommerce.project.controller;
 
 import com.ecommerce.project.config.AppConstants;
-import com.ecommerce.project.model.Category;
 import com.ecommerce.project.payload.CategoryDTO;
 import com.ecommerce.project.payload.CategoryResponse;
 import com.ecommerce.project.service.CategoryService;
@@ -9,10 +8,6 @@ import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.server.ResponseStatusException;
-
-import java.util.ArrayList;
-import java.util.List;
 
 @RestController
 public class CategoryController {
